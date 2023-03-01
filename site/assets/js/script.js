e698444414b9babb0a5f2ee3ddcf61f2b19c933d
Matheus Julian Van Dyke <111263179+matheus-vandyke@users.noreply.github.com>
2023-02-28 23:58:12 -0300

    Corrigido Botões

--- site/assets/js/script.js
+++ site/assets/js/script.js
@@ -43,26 +43,3 @@ for (let i = 0; i < themeBtn.length; i++) {
 }
 
 */
-
-
-// Wait for document to load
-document.addEventListener('DOMContentLoaded', function(event) {
-  document.documentElement.setAttribute("data-theme", "dark");
-
-  // Get our button switcher
-  var themeBtn = document.getElementById('.theme-btn');
-
-  // When our button gets clicked
-  themeBtn.onclick = function() {
-    
-    // Get the current selected theme, on the first run
-    // it should be `light`
-    var currentTheme = document.documentElement.getAttribute("data-theme");
-
-    // Switch between `dark` and `light`
-    var switchToTheme = currentTheme === "dark" ? "light" : "dark"
-
-    // Set our currenet theme to the new one
-    document.documentElement.setAttribute("data-theme", switchToTheme);
-  }
-});
