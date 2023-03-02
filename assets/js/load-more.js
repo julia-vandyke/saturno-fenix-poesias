29f07405a51fec596ece04b34dd5a0a46e991efe
Matheus Julian Van Dyke <111263179+matheus-vandyke@users.noreply.github.com>
2023-03-02 00:52:07 -0300

    Final mudança load-more

--- assets/js/load-more.js
+++ assets/js/load-more.js
@@ -1,21 +1,21 @@
-const loadMoreButton = document.querySelector('.load-more');
-const poemasGroup = document.querySelector('.poemas-group');
-const poemas = poemasGroup.querySelectorAll('.poema');
+const loadMoreButton = document.querySelector(".load-more");
+const poemasGroup = document.querySelector(".poemas-group");
+const poemas = poemasGroup.querySelectorAll(".poema");
 
 let poemasToShow = 10;
 
 function showPoemas() {
-    for (let i = 0; i < poemasToShow.length; i++) {
-        if (i < poemasToShow) {
-            poemas[i].classList.add('show');
-        }
+  for (let i = 0; i < poemas.length; i++) {
+    if (i < poemasToShow) {
+      poemas[i].classList.add("show");
     }
+  }
 }
 
 showPoemas();
 
-loadMoreButton.addEventListener('click', function() {
-    poemasToShow = poemas.length;
-    showPoemas();
-    loadMoreButton.style.display = 'none';
+loadMoreButton.addEventListener("click", function() {
+  poemasToShow = poemas.length;
+  showPoemas();
+  loadMoreButton.style.display = "none";
 });
